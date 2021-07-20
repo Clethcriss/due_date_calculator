39095a990be15b9d26bce5329da73089b4e470eb
Clethcriss <balazs.borsi@guava.blue>
2021-07-20 21:18:27 +0200

    skeleton - service class, calculator method, interface JSDoc

---
+++ dueDateService/dueDateService.ts
@@ -0,0 +1,18 @@
+export interface IDueDateService {
+    calculateDueDate: (submitDate: Date, turnAroundTime: number) => Date;
+}
+
+class DueDateService implements IDueDateService {
+    constructor() {}
+
+    /**
+     * Calculates due date based on the submit date and the turnaround time
+     * @param submitDate
+     * @param turnaroundTime
+     * @returns {Date} - due date
+     */
+    calculateDueDate(submitDate: Date, turnaroundTime: number): Date {
+        return new Date();
+    }
+}
+
