5bb81d5d52c04ec10f7b9fefb7984d730409a1f5
Clethcriss <balazs.borsi@guava.blue>
2021-07-21 23:28:38 +0200

    Basic implementation of the method

--- dueDateService/dueDateService.ts
+++ dueDateService/dueDateService.ts
@@ -12,7 +12,28 @@ class DueDateService implements IDueDateService {
      * @returns {Date} - due date
      */
     calculateDueDate(submitDate: Date, turnaroundTime: number): Date {
-        return new Date();
+        const oneHourInMilliseconds: number = 3600000;
+        const remainingHoursTillEOD: number = 17 - submitDate.getHours();
+
+        let initialDateInMilliseconds: number = submitDate.getTime();
+        let numberOfDaysTillDueDate: number = 0;
+        let remainingHoursOfTurnaroundTime: number;
+        if (turnaroundTime >= 8) {
+            numberOfDaysTillDueDate = Math.floor(turnaroundTime / 8);
+            remainingHoursOfTurnaroundTime = turnaroundTime % 8;
+        } else {
+            remainingHoursOfTurnaroundTime = turnaroundTime;
+        }
+
+        initialDateInMilliseconds += numberOfDaysTillDueDate * 24 * oneHourInMilliseconds;
+
+        if (remainingHoursOfTurnaroundTime < remainingHoursTillEOD) {
+            initialDateInMilliseconds += oneHourInMilliseconds * remainingHoursOfTurnaroundTime;
+        } else {
+            initialDateInMilliseconds += (16 + remainingHoursOfTurnaroundTime) * oneHourInMilliseconds;
+        }
+
+        return new Date(initialDateInMilliseconds);
     }
 }
 
