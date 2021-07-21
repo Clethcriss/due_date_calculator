b167b5113e9524c6a43c5f2309075b871d6fb56f
Clethcriss <balazs.borsi@guava.blue>
2021-07-21 19:55:18 +0200

    Test Proper Input > Friday 9:15, 8 hours turnaround time. Fixed mismatching turnaround times

--- test/dueDateService.test.ts
+++ test/dueDateService.test.ts
@@ -3,7 +3,7 @@ import DueDateService from "../dueDateService/dueDateService";
 describe('Proper input', () => {
     it('It should calculate with submitDate: Monday 9:15, turnaround time: 2 hours', () => {
         const inputSubmitDate: Date = new Date("October 11, 2021 09:15:00");
-        const inputTurnaroundTime: number = 16;
+        const inputTurnaroundTime: number = 2;
 
         const expectedDueDate: Date = new Date("October 11, 2021 11:15:00");
         const dueDateService = new DueDateService();
@@ -12,9 +12,18 @@ describe('Proper input', () => {
 
     it('It should calculate with submitDate: Monday 16:15, turnaround time: 2 hours', () => {
         const inputSubmitDate: Date = new Date("October 11, 2021 09:15:00");
-        const inputTurnaroundTime: number = 16;
+        const inputTurnaroundTime: number = 2;
 
         const expectedDueDate: Date = new Date("October 12, 2021 10:15:00");
+        const dueDateService = new DueDateService();
+        expect(dueDateService.calculateDueDate(inputSubmitDate, inputTurnaroundTime)).toBe(expectedDueDate);
+    });
+
+    it('It should calculate with submitDate: Friday 09:15, turnaround time: 8 hours', () => {
+        const inputSubmitDate: Date = new Date("October 15, 2021 09:15:00");
+        const inputTurnaroundTime: number = 8;
+
+        const expectedDueDate: Date = new Date("October 18, 2021 09:15:00");
         const dueDateService = new DueDateService();
         expect(dueDateService.calculateDueDate(inputSubmitDate, inputTurnaroundTime)).toBe(expectedDueDate);
     });
