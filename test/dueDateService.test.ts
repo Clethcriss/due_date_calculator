f2c2255b7f323a78c345ca2a7e3f72ad857b6256
Clethcriss <balazs.borsi@guava.blue>
2021-07-21 20:27:39 +0200

    Test Proper Input > Wednesday 16:15, 9 hours turnaround time. Fixed mismatching sumbitDate

--- test/dueDateService.test.ts
+++ test/dueDateService.test.ts
@@ -11,7 +11,7 @@ describe('Proper input', () => {
     });
 
     it('It should calculate with submitDate: Monday 16:15, turnaround time: 2 hours', () => {
-        const inputSubmitDate: Date = new Date("October 11, 2021 09:15:00");
+        const inputSubmitDate: Date = new Date("October 11, 2021 16:15:00");
         const inputTurnaroundTime: number = 2;
 
         const expectedDueDate: Date = new Date("October 12, 2021 10:15:00");
@@ -24,6 +24,15 @@ describe('Proper input', () => {
         const inputTurnaroundTime: number = 8;
 
         const expectedDueDate: Date = new Date("October 18, 2021 09:15:00");
+        const dueDateService = new DueDateService();
+        expect(dueDateService.calculateDueDate(inputSubmitDate, inputTurnaroundTime)).toBe(expectedDueDate);
+    });
+
+    it('It should calculate with submitDate: Wednesday 16:15, turnaround time: 9 hours', () => {
+        const inputSubmitDate: Date = new Date("October 13, 2021 16:15:00");
+        const inputTurnaroundTime: number = 9;
+
+        const expectedDueDate: Date = new Date("October 15, 2021 09:15:00");
         const dueDateService = new DueDateService();
         expect(dueDateService.calculateDueDate(inputSubmitDate, inputTurnaroundTime)).toBe(expectedDueDate);
     });
