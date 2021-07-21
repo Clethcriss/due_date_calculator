f2091fa6506b651169bae80ee100d5a10ff6db1a
Clethcriss <balazs.borsi@guava.blue>
2021-07-21 19:50:27 +0200

    Test Proper Input > Monday 16:15, 2 hours turnaround time.

--- test/dueDateService.test.ts
+++ test/dueDateService.test.ts
@@ -9,4 +9,13 @@ describe('Proper input', () => {
         const dueDateService = new DueDateService();
         expect(dueDateService.calculateDueDate(inputSubmitDate, inputTurnaroundTime)).toBe(expectedDueDate);
     });
+
+    it('It should calculate with submitDate: Monday 16:15, turnaround time: 2 hours', () => {
+        const inputSubmitDate: Date = new Date("October 11, 2021 09:15:00");
+        const inputTurnaroundTime: number = 16;
+
+        const expectedDueDate: Date = new Date("October 12, 2021 10:15:00");
+        const dueDateService = new DueDateService();
+        expect(dueDateService.calculateDueDate(inputSubmitDate, inputTurnaroundTime)).toBe(expectedDueDate);
+    });
 });
